795e17ef84654a7ce88623b1063d925f43a1adab
Lau_of_DK <lau.jensen@bestinclass.dk>
2009-02-19 23:50:22 +0100

    Removed obsolete folder

--- src/dk/bestinclass/.sof/main.clj
+++
@@ -1,7 +0,0 @@
-(import '(com.jme.app SimpleGame AbstractGame$ConfigShowMode))
- 
-(doto (proxy [SimpleGame] []
-        (simpleInitGame []
-          (prn :init)))
-  (.setConfigShowMode AbstractGame$ConfigShowMode/AlwaysShow)
-  (.start))
